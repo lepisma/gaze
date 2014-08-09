85d20ad0f1557693ba8f53a78b99c730febc8def
lepisma <abhinav.tushar.vs@gmail.com>
2014-08-09 19:26:48 +0530

    Add Information display
    A text display is shown on top left corner to see the gaze vector values

--- src/gaze.py
+++ src/gaze.py
@@ -16,13 +16,17 @@ ret, frame = vc.read()
 while ret:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
-
+    # Values for scaleFactor = 1.2 and for minNeighbors = 2
+    eyes = eye_cascade.detectMultiScale(gray, 1.2, 2)
+    
     for (x, y, w, h) in eyes:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y : y + h, x : x + w]
         roi_color = frame[y : y + h, x : x + w]
         
+        # Detecting pupil using Hough circle transform
+        # Have to improve this to a better technique
+        # Detecting pupil only in window of the eye
         pupils = cv2.HoughCircles(roi_gray, cv2.cv.CV_HOUGH_GRADIENT, 1, 75, param1 = 50, param2 = 13, minRadius = 0, maxRadius = 0)
         
         if pupils is not None:
@@ -31,11 +35,24 @@ while ret:
 
                 cv2.circle(roi_color, (pupil[0], pupil[1]), 2, (0, 0, 255), 3)
 
+    # Print some message
+    cv2.putText(frame, "Gaze vector :", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 0, 0))
+    cv2.putText(frame, "Feature not added yet", (10, 50), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 0, 255))
+
     cv2.imshow("preview", frame)
 
     ret, frame = vc.read()
 
+    # Exits on Esc
     if cv2.waitKey(20) == 27:
         break
 
 cv2.destroyAllWindows()
+
+
+def find_gaze():
+    """
+    Calculates the gaze position using the given positions of eyes and pupils
+    """
+
+    pass
